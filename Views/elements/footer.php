b6f23d7ac889d86f41d082921d5b332adcab626f
Eosiin <jordan.mabille76@outlook.fr>
2022-06-22 14:32:31 +0200

    Changement

---
+++ Views/elements/footer.php
@@ -0,0 +1,6 @@
+<footer>
+        <p class="text-center pt-5 ">copyright</p>
+    </footer>
+</body>
+
+</html>
--- Views/home.php
+++ Views/home.php
@@ -2,46 +2,8 @@
 require_once "../Controllers/home-controller.php";
 ?>
 
-<!DOCTYPE html>
-<html lang="fr">
+<?php include "elements/header.php"?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;400&display=swap" rel="stylesheet">
-    <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="../Public/css/style.css">
-</head>
-
-<body>
-    <header class="background">
-        <nav class="navbar navbar-expand-lg ">
-            <div class="container-fluid ">
-                <a class="navbar-brand text-light" href="#">Site de rencontre</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link text-light" href="#">Setting</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-light" href="#">Log in</a>
-                        </li>
-
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <h1 class="text-light text-center mt-5">LHP8 rencontres</h1>
-    </header>
     <div class="row">
         <?php
         foreach ($array as $key => $value) { ?>
@@ -99,13 +61,8 @@ require_once "../Controllers/home-controller.php";
                         </div>
                     </div>
                 </div>
-
             <?php } ?>
         </tbody>
     </table>
-    <footer>
-        <p class="text-center pt-5 ">copyright</p>
-    </footer>
-</body>
 
-</html>
+  <?php include "elements/footer.php" ?>
